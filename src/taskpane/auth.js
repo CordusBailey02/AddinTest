44a0c823e77762f59266d39b88997121e3f5cd3c
Cordus Bailey <cordusbailey02@gmail.com>
2026-06-19 17:08:35 -0500

    Fix auth page - remove office.js interference

--- src/taskpane/auth.js
+++ src/taskpane/auth.js
@@ -4,16 +4,17 @@ const msalConfig = {
   auth: {
     clientId: "0c472194-49de-476f-ad1f-8cd689bc60e9",
     authority: "https://login.microsoftonline.com/f285c292-6d27-443d-a1f0-dd70bcc4de16",
-    redirectUri: "https://cordusbailey02.github.io/AddinTest/taskpane.html",
+    redirectUri: "https://cordusbailey02.github.io/AddinTest/auth.html",
   },
   cache: {
     cacheLocation: "sessionStorage",
   },
 };
 
-const msalInstance = new PublicClientApplication(msalConfig);
-
-(async () => {
+async function handleAuth() {
+  const msalInstance = new PublicClientApplication(msalConfig);
   await msalInstance.initialize();
   await msalInstance.handleRedirectPromise();
-})();
+}
+
+handleAuth();
